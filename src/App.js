53265d591c37d1d6db56b94fbdaf2e6e4444dd1f
AndrewOh <ohandre3@msu.edu>
2024-03-26 06:51:01 -0400

    changeimage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './circle.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Make react apps
         </p>
         <a
           className="App-link"
